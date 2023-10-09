24fd0bbdbbbda886dc2635f2e00eab7bb87972e3
urvashiramdasani <urvashi.ramdasani@gmail.com>
2023-10-09 19:46:00 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
 
-class App extends Component {
-    render() {
-        return <div>Todo List</div>
-    }
-}
+const Component = styled.div`
+    display: flex;
+    background-color: #fafafa;
+    min-height: 100vh;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1rem;
+`
+
+const Wrapper = styled.div`
+    width: 100%;
+    max-width: 800px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Header = styled.h1`
+    font-weight: 400;
+    text-transform: uppercase;
+    font-size: 2.5rem;
+`
+
+const App = () => (
+    <Component>
+        <Wrapper>
+            <Header>Todo List</Header>
+        </Wrapper>
+    </Component>
+)
 
 export default App;
